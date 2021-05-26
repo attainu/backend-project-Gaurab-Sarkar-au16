e8bbd826836e9fd178b61249f817a8bcdae3845c
Gaurab-Sarkar-au8 <64783626+Gaurab-Sarkar-au8@users.noreply.github.com>
2021-05-26 11:17:41 +0530

    API COMPLETED

--- app.js
+++ app.js
@@ -1,99 +1,28 @@
-const fs = require('fs')
 const express = require('express')
+const morgan = require('morgan')
+
+const tourRouter = require('./routes/tourRoutes')
+const userRouter = require('./routes/userRoutes')
 
 const app = express()
 
 // MIDDLEWARES
+console.log(process.env.NODE_ENV)
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'))
+}
+
 app.use(express.json())
+app.use(express.static(`${__dirname}/public`))
 
-const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`))
-
-// ROUTE HANDLERS
-const getAllTours = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    results: tours.length,
-    data: {
-      tours: tours
-    }
-  })
-}
-
-const getTour = (req, res) => {
-  console.log(req.params)
-  const id = req.params.id * 1
-  const tour = tours.find(el => el.id === id)
-
-  if (!tour){
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID'
-    })
-  }  
-
-  res.status(200).json({
-    status: 'Success',
-    data: {
-      tour: tour
-    }
-  })
-}
-
-const createTour = (req, res) => {
-  
-  const newId = tours[tours.length - 1].id + 1;
-  const newTour = Object.assign({ id: newId }, req.body)
-
-  tours.push(newTour)
-
-  fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
-    res.status(201).json({
-      status: 'Success',
-      data: {
-        tour: newTour
-      }
-    })
-  })
-}
-
-const updateTour = (req, res) => {
-  if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({
-      status:'fail',
-      message: 'Invalid ID'
-    })
-  }
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour: '<Updated tour..>'
-    }
-  })
-}
-
-const deleteTour = (req, res) => {
-  if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({
-      status:'fail',
-      message: 'Invalid ID'
-    })
-  }
-  res.status(204).json({
-    status: 'success',
-    data: null
-  })
-}
+app.use((req, res, next) => {
+  console.log('middleware working')
+  next()
+})
 
 // ROUTES
-app.get('/api/v1/tours', getAllTours)
-app.get('/api/v1/tours/:id', getTour)
-app.post('/api/v1/tours', createTour)
-app.patch('/api/v1/tours/:id', updateTour)
-app.delete('/api/v1/tours/:id', deleteTour)
-
+app.use('/api/v1/tours', tourRouter)
+app.use('/api/v1/users', userRouter)
 
 // START THE SERVER
-const port = 3100 
-app.listen(port, () => {
-  console.log("Server Started")
-})
+module.exports = app
---
+++ server.js
@@ -0,0 +1,12 @@
+const dotenv = require('dotenv')
+dotenv.config({ path: './config.env'})
+
+const app = require('./app')
+
+
+// console.log(process.env)
+
+const port = process.env.PORT
+app.listen(port, () => {
+  console.log("Server Started")
+})
