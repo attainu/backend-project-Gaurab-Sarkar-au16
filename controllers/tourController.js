d868276da45ef0f45bbc20d4ac3d899bc23ab370
Gaurab-Sarkar-au8 <64783626+Gaurab-Sarkar-au8@users.noreply.github.com>
2021-05-28 11:30:26 +0530

    APPLY QUERY FILTERING

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -2,8 +2,23 @@ const Tour = require('./../models/tourModel')
 
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find()
+    //BUILD QUERY
+    // Filtering
+    const queryObj = {...req.query}
+    const excludeFields = ['page', 'sort', 'limit', 'fields']
+    excludeFields.forEach(el => delete queryObj[el])
+    
+    // Advanced filtering
+    let queryStr = JSON.stringify(queryObj)
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g , match => `$${match}`)
+    console.log(JSON.parse(queryStr))
 
+    const query = Tour.find(JSON.parse(queryStr))
+
+    // EXECUTE QUERY
+    const tours = await query
+
+    //SEND RESPONCE
     res.status(200).json({
       status: 'success',    
       results: tours.length,
